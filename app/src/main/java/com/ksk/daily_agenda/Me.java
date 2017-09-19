aec0a7eb0c77aa320311a28b3fd80f933ed7c34b
Kotjik <31316698+Kotjik@users.noreply.github.com>
2017-09-19 14:44:40 +0200

    Add files via upload

---
+++ app/src/main/java/com/ksk/daily_agenda/Me.java
@@ -0,0 +1,67 @@
+package com.ksk.daily_agenda;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Me extends AppCompatActivity {
+    Button notes;
+    Button calendar;
+    Button fridge;
+    Button finances;
+    Button map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_me);
+        setupUI();
+    }
+
+    private void setupUI(){
+        notes = (Button) findViewById(R.id.me_notes);
+        calendar = (Button) findViewById(R.id.me_calendar);
+        fridge = (Button) findViewById(R.id.me_fridge);
+        finances = (Button) findViewById(R.id.me_finances);
+        map = (Button) findViewById(R.id.me_map);
+        notes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        calendar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        fridge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),FridgeMe.class);
+                startActivity(intent);
+            }
+        });
+        finances.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Finances.class);
+                startActivity(intent);
+            }
+        });
+        map.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri gmmIntentUri = Uri.parse("geo:0,0?q=Mein Standort");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+                mapIntent.setPackage("com.google.android.apps.maps");
+                startActivity(mapIntent);
+
+            }
+        });
+    }
+}
